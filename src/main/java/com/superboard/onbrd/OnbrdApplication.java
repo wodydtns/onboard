dd0081b92d717e8a7c54fb876058251d92131f84
park <wodydtns@naver.com>
2023-01-09 12:50:36 +0900

    feat: setting

---
+++ src/main/java/com/superboard/onbrd/OnbrdApplication.java
@@ -0,0 +1,13 @@
+package com.superboard.onbrd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnbrdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnbrdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/superboard/onbrd/OnbrdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.superboard.onbrd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnbrdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
