578edbd4feb05205c1fb682cea8b85ccf6ee82cd
wodydtns <wodydtns@naver.com>
2023-01-16 23:01:15 +0900

    feat : swagger, querydsl 설정

---
+++ src/main/java/com/superboard/onbrd/global/config/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package com.superboard.onbrd.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import com.fasterxml.classmate.TypeResolver;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@EnableWebMvc
+@Configuration
+public class SwaggerConfig extends WebMvcConfigurationSupport{
+
+	@Bean
+    public Docket api(TypeResolver typeResolver) {
+        return new Docket(DocumentationType.OAS_30) // 3.0 문서버전으로 세팅
+                .useDefaultResponseMessages(true)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.sample.swagger.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Swagger 3.0 Api Sample")
+                .description("This is Sample")
+                .version("1.0")
+                .build();
+    }
+}
