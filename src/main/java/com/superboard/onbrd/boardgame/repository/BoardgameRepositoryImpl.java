5d708c88e0fbbed34f25382d223eb05ed2a1ea89
wodydtns <wodydtns@naver.com>
2023-02-18 12:33:52 +0900

    feat : where 절 수정

--- src/main/java/com/superboard/onbrd/boardgame/repository/BoardgameRepositoryImpl.java
+++ src/main/java/com/superboard/onbrd/boardgame/repository/BoardgameRepositoryImpl.java
@@ -13,15 +13,18 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.ObjectUtils;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.superboard.onbrd.boardgame.dto.BoardgameDetailDto;
 import com.superboard.onbrd.boardgame.dto.BoardgameSearchByTagRequest;
 import com.superboard.onbrd.boardgame.dto.BoardgameSearchByTagResponse;
+import com.superboard.onbrd.boardgame.dto.BoardgameSearchByTagResponse.BoardGameResponse;
 import com.superboard.onbrd.boardgame.entity.Boardgame;
 import com.superboard.onbrd.boardgame.entity.QBoardgame;
-import com.superboard.onbrd.tag.entity.QBoardgameTag;
 import com.superboard.onbrd.tag.entity.QTag;
 import com.superboard.onbrd.tag.entity.Tag;
 
@@ -34,20 +37,21 @@ public class BoardgameRepositoryImpl implements BoardgameRepository {
 	private final JPAQueryFactory queryFactory;
 
 	@Override
-	public Page<BoardgameSearchByTagResponse.BoardGameResponse> searchBoardgameByRecommand(BoardgameSearchByTagRequest boardgameSearchByTagRequest,
-			Pageable pageable) {
-		QBoardgame boardgame = QBoardgame.boardgame;
-		QBoardgameTag boardgameTag = QBoardgameTag.boardgameTag;
-		 List<BoardgameSearchByTagResponse.BoardGameResponse> results = queryFactory.
-				 select(Projections.fields(BoardgameSearchByTagResponse.BoardGameResponse.class,
-				 boardgame.id,boardgame.name,boardgame.image))
-				 .from(boardgameTag).join(boardgameTag.boardgame,boardgame)
-				 .join(boardgameTag.tag,tag)
-				 .where(tag.id.in(boardgameSearchByTagRequest.getTagIds()))
-				 .orderBy(tag.id.asc())
-				 .offset(pageable.getOffset())
-				 .limit(pageable.getPageSize())
-				 .fetch();
+	public Page<BoardgameSearchByTagResponse.BoardGameResponse> searchBoardgameByRecommand(
+			BoardgameSearchByTagRequest boardgameSearchByTagRequest, Pageable pageable) {
+		
+		JPAQuery<BoardgameSearchByTagResponse.BoardGameResponse> query = queryFactory
+				.select(Projections.fields(BoardgameSearchByTagResponse.BoardGameResponse.class, boardgame.id,
+						boardgame.name, boardgame.image))
+				.from(boardgameTag).join(boardgameTag.boardgame, boardgame).join(boardgameTag.tag, tag)
+				.orderBy(tag.id.asc())
+				.offset(pageable.getOffset()).limit(pageable.getPageSize());
+		
+		if(!ObjectUtils.isEmpty(boardgameSearchByTagRequest.getTagIds())) {
+			query.where(tag.id.in(boardgameSearchByTagRequest.getTagIds()));
+		}
+		List<BoardgameSearchByTagResponse.BoardGameResponse> results = query.fetch();
+			
 		return new PageImpl<BoardgameSearchByTagResponse.BoardGameResponse>(results, pageable, results.size());
 	}
 
@@ -76,5 +80,7 @@ public class BoardgameRepositoryImpl implements BoardgameRepository {
 	public Optional<Boardgame> findById(Long id) {
 		return Optional.of(queryFactory.select(boardgame).from(boardgame).where(boardgame.id.eq(id)).fetchFirst());
 	}
+	
+	
 
 }
