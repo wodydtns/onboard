63f65034371e59a96da9a7cbbd0858db0b1768a7
조성호 saintho <saintho95@gmail.com>
2023-02-28 19:13:39 +0900

    feat: auth mail template (#41) (#78)

--- src/main/java/com/superboard/onbrd/mail/service/MailProperties.java
+++ src/main/java/com/superboard/onbrd/mail/service/MailProperties.java
@@ -2,4 +2,7 @@ package com.superboard.onbrd.mail.service;
 
 public class MailProperties {
 	public static final String MAIL_DEFAULT_ENCODING = "UTF-8";
+
+	public static final String LOGO_CID = "logo";
+	public static final String LOGO_IMAGE_PATH = "static/images/logo.png";
 }
