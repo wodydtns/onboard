793d74fa3bdcb66ba52cecd4746b168ac7a6f7a2
park jae wook <wodydtns@naver.com>
2023-03-23 21:04:48 +0900

    feat : 댓글이 달릴 시 push  메시지 송출하는 기능 구현
    - push 메시지 양식 필요
    - token을 통해 테스트 진행 필요

--- src/main/java/com/superboard/onbrd/review/repository/CustomCommentRepositoryImpl.java
+++ src/main/java/com/superboard/onbrd/review/repository/CustomCommentRepositoryImpl.java
@@ -1,23 +1,35 @@
 package com.superboard.onbrd.review.repository;
 
+import com.google.firebase.messaging.FirebaseMessagingException;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.superboard.onbrd.global.util.FCMUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import static com.superboard.onbrd.member.entity.QMember.*;
 import static com.superboard.onbrd.review.entity.QReview.*;
 import static com.superboard.onbrd.review.entity.QComments.*;
-import static com.superboard.onbrd.oauth2.entity.QOauthID.*;
+import static com.superboard.onbrd.auth.entity.QToken.*;
 @Repository
 @RequiredArgsConstructor
 public class CustomCommentRepositoryImpl implements CustomCommentRepository{
 
     private final JPAQueryFactory queryFactory;
 
+    private final FCMUtil fcmUtil;
+
     @Override
     public void selectOauthIdForPushMessage(long createdId) {
-        queryFactory.select(member.id, oauthID.oauthId).from(member)
-                .join(member,oauthID.member)
-                .join(review.writer,member)
-                .fetch();
+        Long memberId = queryFactory.select(member.id).from(comments)
+                .join(comments.review, review)
+                .join(review.writer, member)
+                .where(comments.id.eq(createdId))
+                .fetchOne();
+        String refreshToken = queryFactory.select(token.refreshToken).from(token).where(token.id.eq(memberId)).fetchOne();
+
+        try {
+            fcmUtil.sendAndroidMessage(refreshToken,"FCM title","fcm Body");
+        } catch (FirebaseMessagingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
