30b3f0080c86be08c0e42e3c79e7ee866c0cb17d
toneofrain <toneofrain@gmail.com>
2023-03-31 16:03:20 +0900

    fix: 로그인

--- src/main/java/com/superboard/onbrd/auth/service/AuthServiceImpl.java
+++ src/main/java/com/superboard/onbrd/auth/service/AuthServiceImpl.java
@@ -1,7 +1,6 @@
 package com.superboard.onbrd.auth.service;
 
 import static com.superboard.onbrd.global.exception.ExceptionCode.*;
-import static com.superboard.onbrd.member.entity.MemberStatus.*;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
@@ -19,7 +18,6 @@ import com.superboard.onbrd.auth.dto.TokenDto;
 import com.superboard.onbrd.auth.entity.Token;
 import com.superboard.onbrd.auth.util.AuthCodeMailProvider;
 import com.superboard.onbrd.global.exception.BusinessLogicException;
-import com.superboard.onbrd.global.exception.OnbrdAssert;
 import com.superboard.onbrd.mail.dto.MailSendingEvent;
 import com.superboard.onbrd.member.entity.Member;
 import com.superboard.onbrd.member.entity.Password;
@@ -44,9 +42,9 @@ public class AuthServiceImpl implements AuthService {
 		Password password = passwordService.findVerifiedOneByMember(member);
 		passwordService.validatePassword(request.getPassword(), password.getEncodedPassword());
 
-		OnbrdAssert.state(member.getStatus() == SUSPENDED, SUSPENDED_MEMBER);
-		OnbrdAssert.state(member.getStatus() == WITHDRAWN, WITHDRAWN_MEMBER);
-		OnbrdAssert.state(member.getStatus() == KICKED, KICKED_MEMBER);
+		// OnbrdAssert.state(member.getStatus() == SUSPENDED, SUSPENDED_MEMBER);
+		// OnbrdAssert.state(member.getStatus() == WITHDRAWN, WITHDRAWN_MEMBER);
+		// OnbrdAssert.state(member.getStatus() == KICKED, KICKED_MEMBER);
 
 		member.attendAt(LocalDateTime.now());
 		TokenDto tokens = tokenService.issueTokens(member);
