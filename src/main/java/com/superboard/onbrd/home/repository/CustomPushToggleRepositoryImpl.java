335d8e76ac6e16bb9fb09347a4200476bbd455d6
toneofrain <toneofrain@gmail.com>
2023-04-05 17:33:49 +0900

    fix: PushToggle

--- src/main/java/com/superboard/onbrd/home/repository/CustomPushToggleRepositoryImpl.java
+++ src/main/java/com/superboard/onbrd/home/repository/CustomPushToggleRepositoryImpl.java
@@ -1,38 +1,39 @@
 package com.superboard.onbrd.home.repository;
 
+import static com.superboard.onbrd.home.entity.QPushToggle.*;
+import static com.superboard.onbrd.member.entity.QMember.*;
+
+import org.springframework.stereotype.Repository;
+
 import com.querydsl.core.types.Projections;
-import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.superboard.onbrd.home.dto.PushToggleDto;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
-import org.springframework.util.StringUtils;
 
-import static com.superboard.onbrd.home.entity.QpushToggle.*;
-import static com.superboard.onbrd.member.entity.QMember.*;
+import lombok.RequiredArgsConstructor;
+
 @Repository
 @RequiredArgsConstructor
 public class CustomPushToggleRepositoryImpl implements CustomPushToggleRepository {
 
-    private final JPAQueryFactory queryFactory;
+	private final JPAQueryFactory queryFactory;
 
+	@Override
+	public PushToggleDto getPushToggle(long id) {
+		return queryFactory.select(
+				Projections.fields(PushToggleDto.class, pushToggle.id, pushToggle.commentYn, pushToggle.favoriteTagYn))
+			.from(pushToggle).join(pushToggle.member, member)
+			.where(member.id.eq(id))
+			.fetchOne();
+	}
 
-    @Override
-    public PushToggleDto getPushToggle(long id) {
-        return queryFactory.select(Projections.fields(PushToggleDto.class, pushToggle.id,pushToggle.commentYn,pushToggle.favoriteTagYn))
-                .from(pushToggle).join(pushToggle.member,member)
-                .where(member.id.eq(id))
-                .fetchOne();
-    }
+	@Override
+	public Long updatePushToggle(PushToggleDto dto) {
+		return queryFactory.update(pushToggle)
+			.set(pushToggle.commentYn, dto.getCommentYn())
+			.set(pushToggle.favoriteTagYn, dto.getFavoriteTagYn())
+			.where(member.id.eq(dto.getId()))
+			.execute();
 
-    @Override
-    public Long updatePushToggle(PushToggleDto dto) {
-        return queryFactory.update(pushToggle)
-                .set(pushToggle.commentYn , dto.getCommentYn())
-                .set(pushToggle.favoriteTagYn, dto.getFavoriteTagYn())
-                .where(member.id.eq(dto.getId()))
-                .execute();
-
-    }
+	}
 
 }
