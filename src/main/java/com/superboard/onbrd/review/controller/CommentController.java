9a338597b77e18323809f187845548776e89b7e2
toneofrain <toneofrain@gmail.com>
2023-04-12 15:42:39 +0900

    fix: get comments by review Id

--- src/main/java/com/superboard/onbrd/review/controller/CommentController.java
+++ src/main/java/com/superboard/onbrd/review/controller/CommentController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -105,6 +106,7 @@ public class CommentController {
 		@ApiResponse(responseCode = "200"),
 		@ApiResponse(responseCode = "404")
 	})
+	@GetMapping
 	public ResponseEntity<OnbrdListResponse<CommentDetail>> getCommentsByReviewId(@PathVariable Long reviewId) {
 		OnbrdListResponse<CommentDetail> response = commentService.getCommentsByReviewId(reviewId);
 
