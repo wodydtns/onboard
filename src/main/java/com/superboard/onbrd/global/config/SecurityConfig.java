b3c89c3fa5366bedc78c08846b3aaf0f79022086
toneofrain <toneofrain@gmail.com>
2023-06-01 20:24:36 +0900

    fix: security

--- src/main/java/com/superboard/onbrd/global/config/SecurityConfig.java
+++ src/main/java/com/superboard/onbrd/global/config/SecurityConfig.java
@@ -19,7 +19,6 @@ import com.superboard.onbrd.auth.filter.JwtAuthenticationFilter;
 import com.superboard.onbrd.auth.util.JwtTokenProvider;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -28,55 +27,58 @@ public class SecurityConfig {
 	private final JwtTokenProvider jwtTokenProvider;
 
 	@Bean
-		public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-			http.httpBasic()
-					.disable()
-					.formLogin()
-					.disable()
-					.sessionManagement()
-					.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-					.and()
-					.addFilterBefore(
-							new JwtAuthenticationFilter(jwtTokenProvider),
-							UsernamePasswordAuthenticationFilter.class)
-					.csrf()
-					.disable()
-					.headers()
-					.frameOptions()
-					.disable()
-					.and()
-					.exceptionHandling()
-					.authenticationEntryPoint(unauthorizedExceptionEntryPoint())
-					.and()
-					.authorizeRequests()
-					.mvcMatchers("/api/**/admin/**").hasRole("ADMIN")
-					.mvcMatchers("/api/**/members/{memberId:[0-9]+}/authority").permitAll()
-					// .mvcMatchers("/api/*/admin/**").authenticated()
-					.mvcMatchers("/h2/**").permitAll()
-					.mvcMatchers("/api/*/auth/token-reissue").permitAll()
-					.mvcMatchers(GET, "/api/*/boardgames/{boardgameId:[0-9]+}/reviews").permitAll()
-					.mvcMatchers(GET, "/api/*/tags").permitAll()
-					.mvcMatchers(DELETE, "/api/*/members/{memberId:[0-9]+}").permitAll()
-					.mvcMatchers(POST, "/api/*/members/sign-up").permitAll()
-					.mvcMatchers(GET,
-							"/api/*/members/nickname-check", "/api/*/members/mail-check").permitAll()
-					.mvcMatchers(POST, "/api/*/auth/sign-in").permitAll()
-					.mvcMatchers(GET, "/api/*/auth/code").permitAll()
-					.mvcMatchers(POST, "/api/*/auth/code-resending", "/api/*/auth/code-check").permitAll()
-					.mvcMatchers(PATCH, "/api/*/passwords").permitAll()
-					.mvcMatchers(GET, "/api/*/passwords/code").permitAll()
-					.mvcMatchers(POST, "api/*/passwords/code-check").permitAll()
-					.mvcMatchers(GET, "api/*/boardgames/curation").permitAll()
-					.mvcMatchers(GET, "/api/*/members/{memberId:[0-9]+}").permitAll()
-					.mvcMatchers(GET, "/api/*/boardgames/{boardgameId:[0-9]+}").permitAll()
-					.mvcMatchers(GET, "/api/*/boardgames/curation").permitAll()
-					.mvcMatchers(GET, "/api/*/boardgames/searchBoardgameList").permitAll()
-					.mvcMatchers(GET, "/api/*/boardgames/top10").permitAll()
-					.and()
-					.authorizeRequests()
-					.antMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-resources/**")
-					.permitAll();
-			return http.build();
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.httpBasic()
+			.disable()
+			.formLogin()
+			.disable()
+			.sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+			.addFilterBefore(
+				new JwtAuthenticationFilter(jwtTokenProvider),
+				UsernamePasswordAuthenticationFilter.class)
+			.csrf()
+			.disable()
+			.headers()
+			.frameOptions()
+			.disable()
+			.and()
+			.exceptionHandling()
+			.authenticationEntryPoint(unauthorizedExceptionEntryPoint())
+			.and()
+			.authorizeRequests()
+			.mvcMatchers("/api/**/admin/**").hasRole("ADMIN")
+			.mvcMatchers("/api/**/members/{memberId:[0-9]+}/authority").permitAll()
+			// .mvcMatchers("/api/*/admin/**").authenticated()
+			.mvcMatchers("/h2/**").permitAll()
+			.mvcMatchers("/api/*/auth/token-reissue").permitAll()
+			.mvcMatchers(GET, "/api/*/boardgames/{boardgameId:[0-9]+}/reviews").permitAll()
+			.mvcMatchers(GET, "/api/*/tags").permitAll()
+			.mvcMatchers(DELETE, "/api/*/members/{memberId:[0-9]+}").permitAll()
+			.mvcMatchers(POST, "/api/*/members/sign-up").permitAll()
+			.mvcMatchers(GET,
+				"/api/*/members/nickname-check", "/api/*/members/mail-check").permitAll()
+			.mvcMatchers(POST, "/api/*/auth/sign-in").permitAll()
+			.mvcMatchers(GET, "/api/*/auth/code").permitAll()
+			.mvcMatchers(POST, "/api/*/auth/code-resending", "/api/*/auth/code-check").permitAll()
+			.mvcMatchers(PATCH, "/api/*/passwords").permitAll()
+			.mvcMatchers(GET, "/api/*/passwords/code").permitAll()
+			.mvcMatchers(POST, "api/*/passwords/code-check").permitAll()
+			.mvcMatchers(GET, "api/*/boardgames/curation").permitAll()
+			.mvcMatchers(GET, "/api/*/members/{memberId:[0-9]+}").permitAll()
+			.mvcMatchers(GET, "/api/*/boardgames/{boardgameId:[0-9]+}").permitAll()
+			.mvcMatchers(GET, "/api/*/boardgames/curation").permitAll()
+			.mvcMatchers(GET, "/api/*/boardgames/searchBoardgameList").permitAll()
+			.mvcMatchers(GET, "/api/*/boardgames/top10").permitAll()
+			.and()
+			.authorizeRequests()
+			.antMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-resources/**")
+			.permitAll()
+			.anyRequest()
+			.authenticated();
+
+		return http.build();
 	}
 
 	@Bean
