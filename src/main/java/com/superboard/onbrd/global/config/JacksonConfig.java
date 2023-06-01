646c79883f30288233ceb8fcdacc5b48af504074
toneofrain <toneofrain@gmail.com>
2023-06-02 02:53:00 +0900

    fix: Notice

--- src/main/java/com/superboard/onbrd/global/config/JacksonConfig.java
+++ src/main/java/com/superboard/onbrd/global/config/JacksonConfig.java
@@ -4,12 +4,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @Configuration
 public class JacksonConfig {
 
 	@Bean
 	public ObjectMapper objectMapper() {
-		return new ObjectMapper();
+		ObjectMapper mapper = new ObjectMapper();
+
+		mapper.registerModule(new JavaTimeModule());
+		mapper.enable(SerializationFeature.INDENT_OUTPUT);
+		mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+		return mapper;
 	}
 }
--- src/main/java/com/superboard/onbrd/notice/dto/NoticeDetail.java
+++ src/main/java/com/superboard/onbrd/notice/dto/NoticeDetail.java
@@ -3,8 +3,10 @@ package com.superboard.onbrd.notice.dto;
 import java.time.LocalDateTime;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class NoticeDetail {
 	private Long id;
 	private String title;
